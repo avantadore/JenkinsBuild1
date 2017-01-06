b9b26b0a8c7d8a1d0bd84b1ca846586b5d33b486
Jan <jls@lsit.dk>
2017-01-06 08:34:13 +0100

    Added new project and test cases

---
+++ JenkinsBuild/Common.Test/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenStringAnalyzer_WhenInputIsAFoobar_ThenReturnTrue()
+        {
+            // Arrange
+            var s = "ThisIsAFoobarString";
+            var sut = new StringAnalyzer();
+
+            // Act
+            var result = sut.IsStringFooBar(s);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GivenStringAnalyzer_WhenInputIsNotAFoobar_ThenReturnFalse()
+        {
+            // Arrange
+            var s = "ThisIsAString";
+            var sut = new StringAnalyzer();
+
+            // Act
+            var result = sut.IsStringFooBar(s);
+
+            // Assert
+            Assert.IsTrue(!result);
+        }
+
+        [TestMethod]
+        public void GivenStringAnalyzer_WhenStringIsPalindrome_ThenReturnTrue()
+        {
+            // Arrange
+            var s = "murmellemrum";
+            var sut = new StringAnalyzer();
+
+            // Act
+            var result = sut.IsPalindrome(s);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GivenStringAnalyzer_WhenStringIsNotPalindrome_ThenReturnFalse()
+        {
+            // Arrange
+            var s = "OhNo";
+            var sut = new StringAnalyzer();
+
+            // Act
+            var result = sut.IsPalindrome(s);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+    }
+}
---
+++ JenkinsBuild/Common/StringAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class StringAnalyzer
+    {
+        public bool IsStringFooBar(string input)
+        {
+            return input.ToUpper().Contains("FOOBAR");
+        }
+
+        public bool IsPalindrome(string input)
+        {
+            return input.SequenceEqual(input.Reverse());
+        }
+    }
+}
